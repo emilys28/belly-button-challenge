0cd9cda89cc14adb28fed7dcbb6789a262b0e8cb
emilys28 <esims28.97@gmail.com>
2024-02-10 00:37:57 -0500

    Read in sample.json data
    began working bar char

--- static/js/app.js
+++ static/js/app.js
@@ -1,16 +1,60 @@
-// Use the D3 library to read in samples.json from the URL
+// 1. Use the D3 library to read in samples.json from the URL
 
-https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json
+    //creating constant variable URL
+    const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+    //reading then displaying data in console log
+    d3.json(url).then(function(data) {
+        console.log(data);
+    });
 
-//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Use sample_values as the values for the bar chart.
 // Use otu_ids as the labels for the bar chart.
 // Use otu_labels as the hovertext for the chart.
 
+    // Drop down menu: 
+   
 
 
-// Create a bubble chart that displays each sample.
+    // Sort the data by descending
+    let sortedsamples = samples.sort((a, b) => b.samples - a.samples);
+
+    // Slice the first 10 objects for plotting
+    let slicedData = sortedsamples.slice(0, 10);
+
+    // Reverse the array to accommodate Plotly's defaults
+    slicedData.reverse();
+
+    // Trace1 for the Sample Data
+    let trace1 = {
+        x: slicedData.map(object => object.sample_values),
+        y: slicedData.map(object => object.otu_ids),
+        text: slicedData.map(object => object.otu_labels),
+        name: "Labels",
+        type: "bar",
+        orientation: "h"
+    };
+
+    // Data array
+    let data = [trace1];
+
+    // Apply a title to the layout
+    let layout = {
+    title: "top 10 OTUs found in ${id}",
+    margin: {
+        l: 100,
+        r: 100,
+        t: 100,
+        b: 100
+    }
+    };
+
+    // Render the plot to the div tag with id "plot"
+    Plotly.newPlot("plot", data, layout);
+
+
+// 3.  Create a bubble chart that displays each sample.
 // Use otu_ids for the x values.
 // Use sample_values for the y values.
 // Use sample_values for the marker size.
@@ -18,19 +62,19 @@ https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Inter
 // Use otu_labels for the text values.
 
 
-// Display the sample metadata, i.e., an individual's demographic information.
+// 4. Display the sample metadata, i.e., an individual's demographic information.
 
 
 
-// Display each key-value pair from the metadata JSON object somewhere on the page.
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
 
 
 
-// Update all the plots when a new sample is selected. 
+// 6. Update all the plots when a new sample is selected. 
 // Additionally, you are welcome to create any layout that you would like for your dashboard. 
 
 
-// Deploy your app to a free static page hosting service, such as GitHub Pages. 
+// 7. Deploy your app to a free static page hosting service, such as GitHub Pages. 
 // Submit the links to your deployment and your GitHub repo. 
 // Ensure that your repository has regular commits and a thorough README.md file
 
