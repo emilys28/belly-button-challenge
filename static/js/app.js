7068a5d14991d23fef6c800af96291e77621a32b
emilys28 <esims28.97@gmail.com>
2024-02-11 20:48:55 -0500

    solving first two reqs.

--- static/js/app.js
+++ static/js/app.js
@@ -1,57 +1,67 @@
 // 1. Use the D3 library to read in samples.json from the URL
 
-    //creating constant variable URL
-    const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+//creating constant variable URL
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-    //reading then displaying data in console log
-    d3.json(url).then(function(data) {
-        console.log(data);
-    });
+//reading then displaying data in console log
+d3.json(url).then(function(data) {
+     console.log(data);
+});
 
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 // Use sample_values as the values for the bar chart.
 // Use otu_ids as the labels for the bar chart.
 // Use otu_labels as the hovertext for the chart.
 
-    // Drop down menu: 
-   
+// Function to initialize the page
+function init() {
+    let dropdownMenu = d3.select("#selDataset");
+    // Read data from the JSON file
+    d3.json(url).then(function(data) {
+        // Extract sample IDs from data
+        const sampleIds = data.names;
+        
+        // dropdown menu with sample IDs
+        sampleIds.forEach((id) => {
+            dropdownMenu.append("option").text(id).property("value", id);
+        });
+
+        // Display the first sample by default
+        let firstSample = sampleIds[0];
+        BarCharts(firstSample);
+        BubbleCharts(firstSample);
+    });
+}
 
 
-    // Sort the data by descending
-    let sortedsamples = samples.sort((a, b) => b.samples - a.samples);
+// Function to build BAR charts
+function BarCharts(sampleId) {
+    d3.json(url).then(function(data) {
+        // Filter data for the selected sample
+        const sample = data.samples.find(sample => sample.id === sampleId);
+        
+        // Extract top 10 OTUs
+        let otuIds = sample.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
+        let sampleValues = sample.sample_values.slice(0, 10).reverse();
+        let otuLabels = sample.otu_labels.slice(0, 10).reverse();
 
-    // Slice the first 10 objects for plotting
-    let slicedData = sortedsamples.slice(0, 10);
-
-    // Reverse the array to accommodate Plotly's defaults
-    slicedData.reverse();
-
-    // Trace1 for the Sample Data
-    let trace1 = {
-        x: slicedData.map(object => object.sample_values),
-        y: slicedData.map(object => object.otu_ids),
-        text: slicedData.map(object => object.otu_labels),
-        name: "Labels",
-        type: "bar",
-        orientation: "h"
-    };
-
-    // Data array
-    let data = [trace1];
-
-    // Apply a title to the layout
-    let layout = {
-    title: "top 10 OTUs found in ${id}",
-    margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-    }
-    };
-
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("plot", data, layout);
+        // Create horizontal bar chart
+        let trace1 = {
+            x: sampleValues,
+            y: otuIds,
+            text: otuLabels,
+            type: "bar",
+            orientation: "h"
+        };
+        let layout1 = {
+            title: "Top 10 OTUs",
+            xaxis: { title: "Sample Values" },
+            yaxis: { title: "OTU IDs" }
+        };
+        Plotly.newPlot("bar", [trace1], layout1);
+    });
+}
+    
 
 
 // 3.  Create a bubble chart that displays each sample.
@@ -60,6 +70,38 @@
 // Use sample_values for the marker size.
 // Use otu_ids for the marker colors.
 // Use otu_labels for the text values.
+
+// Function to build BUBBLE chart
+function BubbleCharts(sampleId) {
+    d3.json(url).then(function(data) {
+        // Filter data for the selected sample
+        const sample = data.samples.find(sample => sample.id === sampleId);
+        
+        // Extract top 10 OTUs
+        let otuIds = sample.otu_ids;
+        let sampleValues = sample.sample_values;
+        let otuLabels = sample.otu_labels;
+
+        // Create horizontal bar chart
+        let trace2 = {
+            x: otuIds,
+            y: sampleValues,
+            text: otuLabels,
+            mode: 'markers',
+            marker: {
+                size: sampleValues,
+                color: otuIds,
+                colorscale: 'Earth'
+            }
+        };
+        let layout2 = {
+            title: 'Sample Bubble Chart',
+            xaxis: { title: 'OTU IDs' },
+            yaxis: { title: 'Sample Values' }
+        };
+        Plotly.newPlot("bubble", [trace2], layout2);
+    });
+}
 
 
 // 4. Display the sample metadata, i.e., an individual's demographic information.
@@ -88,3 +130,11 @@
 // Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ to plot the weekly washing frequency of the individual.
 // You will need to modify the example gauge code to account for values ranging from 0 through 9.
 // Update the chart whenever a new sample is selected.
+
+// Event listener for dropdown change
+function optionChanged(newSampleId) {
+    buildCharts(newSampleId);
+}
+
+// Initialize the page
+init();
